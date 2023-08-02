1bf93f486e24fb7820635d01d97cd2a7f83bb2a6
salmalah <s.salmalahmidi@gmail.com>
2023-08-02 09:25:09 -0700

    fix betty errors

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -11,7 +11,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (!tree || !func)
 		return;
 	func(tree->n);
-	binary_tree_preorder(tree->right, func);
 	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
 
