ec1b75615727c716b7ccf2b14612ae773432a107
salmalah <s.salmalahmidi@gmail.com>
2023-08-02 09:23:55 -0700

    fix betty errors

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,16 +1,17 @@
 #include "binary_trees.h"
 /**
- * binary_tree_preorder - A functionGoes through a binary tree using pre-order traversal
+ * binary_tree_preorder - A function Goes through
+ * a binary tree using pre-order traversal
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node's value
  * Return: Nothing
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (!tree || !func)
-        return;
-    func(tree->n);
-    binary_tree_preorder(tree->left, func);
-    binary_tree_preorder(tree->right, func);
+	if (!tree || !func)
+		return;
+	func(tree->n);
+	binary_tree_preorder(tree->right, func);
+	binary_tree_preorder(tree->left, func);
 }
 
