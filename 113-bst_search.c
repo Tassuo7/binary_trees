e28e6f2b33842e11c2e206ea9246a22c0b5fee9a
Tassuo7 <abdelhamid.jack@gmail.com>
2023-08-03 14:06:32 -0700

    fix betty error

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -9,13 +9,11 @@ bst_t *bst_search(const bst_t *tree, int value)
 {
 	if (!tree)
 		return (NULL);
-	else
-	{
-		if (tree->n == value)
-			return ((bst_t *)tree);
-		if (tree->n > value)
-			return (bst_search(tree->left, value));
 
-		return (bst_search(tree->right, value));
-	}
+	if (tree->n == value)
+		return ((bst_t *)tree);
+	if (tree->n > value)
+		return (bst_search(tree->left, value));
+
+	return (bst_search(tree->right, value));
 }
