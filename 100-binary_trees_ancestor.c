29a2deac14cf3cdcf258bdc3dbd158f493f9291f
salmalah <s.salmalahmidi@gmail.com>
2023-08-03 10:57:28 -0700

    fix error

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -11,12 +11,13 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 {
 	if (!first || !second)
 		return (NULL);
-	if (first == second->parent || first == second || first->parent == second)
+	if (first == second)
 		return ((binary_tree_t *)first);
-	if (!first->parent || !second->parent)
-		return (NULL);
-	if (first->parent == second->parent)
-		return (first->parent);
+	if (first == second->parent || !first->parent ||
+			(!first->parent->parent && second->parent))
+		return (binary_trees_ancestor(first, second->parent));
+	else
+		return (binary_trees_ancestor(first->parent, second));
 	return (binary_trees_ancestor(first->parent, second->parent));
 }
 
